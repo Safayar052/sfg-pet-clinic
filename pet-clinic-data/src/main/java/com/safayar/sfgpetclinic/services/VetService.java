b67f41868c7e175f5b1a69db5c2ad19c34e0b825
STIHOCOM\sahmed <safayar@sybrandsplace.nl>
2018-12-03 18:37:01 +0100

    add common crud service

--- pet-clinic-data/src/main/java/com/safayar/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/safayar/sfgpetclinic/services/VetService.java
@@ -4,9 +4,6 @@ import com.safayar.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
     Vet findByLastName(String lastName);
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
